532382c18cc1b9431695d7f8d8e31844ac0faa90
Gabriel S. Jesus <119432493+NotedSilva@users.noreply.github.com>
2023-06-05 14:43:40 -0300

    commit inicial
    O primeiro commit do site

---
+++ javinha.js
@@ -0,0 +1,16 @@
+var botao1 = document.getElementById("btn1")
+var botao2 = document.getElementById("btn2")
+var botao3 = document.getElementById("btn3")
+
+btn1.addEventListener("click", function() {
+     document.body.style.backgroundColor = "red";
+});
+
+btn2.addEventListener("click", function() {
+    document.body.style.backgroundColor = "green";
+});
+
+btn3.addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
+
